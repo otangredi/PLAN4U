a7cdda1c74e1c188609a3d7af10015c3b867dcba
hanbyeol kwon <janviertrois@naver.com>
2022-11-22 14:04:38 -0500

    Added New, Create method for event controller

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,27 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_223527) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "events", force: :cascade do |t|
+    t.date "date"
+    t.string "venue"
+    t.string "partner_name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
+  create_table "guests", force: :cascade do |t|
+    t.string "name"
+    t.string "relationship"
+    t.string "email"
+    t.integer "status"
+    t.bigint "event_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_guests_on_event_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -27,4 +48,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_223527) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users"
+  add_foreign_key "guests", "events"
 end
