e36b0df45c1025fb81fe7089f51ec3d999d8a053
hanbyeol kwon <janviertrois@naver.com>
2022-11-28 17:54:44 -0500

    table being created, one table displaying part(circle) needs more work

--- app/controllers/guest_tables_controller.rb
+++ app/controllers/guest_tables_controller.rb
@@ -5,13 +5,39 @@ class GuestTablesController < ApplicationController
   end
 
   def new
-    @table = GuestTable.new
     @event = Event.find(params[:event_id])
+    @table = GuestTable.new
   end
 
   def create
     @event = Event.find(params[:event_id])
     @table = GuestTable.new(table_params)
+
+    #Family_B Table Making
+    guests = @event.guests.where(relationship: "Family_B").map { |g| g.name }
+    connection_1 = [@event.guests.where(relationship: "Family_B").map do |guest|
+                    {people: guest.guest_choice.choices, weight: 2}
+                    end ]
+    connection_2 = [@event.guests.where(relationship: "Family_B").map do |guest|
+                    {people: guests - guest.guest_choice.choices + [guest.name], weight: 1}
+                    end ]
+    connections_3 = connection_1.flatten + connection_2.flatten
+    tables = [5, 5]
+    seating_1 = ORTools::Seating.new(connections: connections_3.flatten, tables: tables)
+    @table.tables += seating_1.assigned_tables
+
+    guests = @event.guests.where(relationship: "Family_G").map { |g| g.name }
+    connection_4 = [@event.guests.where(relationship: "Family_G").map do |guest|
+                    {people: guest.guest_choice.choices, weight: 2}
+                    end ]
+    connection_5 = [@event.guests.where(relationship: "Family_G").map do |guest|
+                    {people: guests - guest.guest_choice.choices + [guest.name], weight: 1}
+                    end ]
+    connections_6 = connection_4.flatten + connection_5.flatten
+    tables = [5, 5]
+    seating_2 = ORTools::Seating.new(connections: connections_6.flatten, tables: tables)
+    @table.tables += seating_2.assigned_tables
+
     @table.event = @event
     if @table.save!
       redirect_to event_guest_table_path(@event, @table)
@@ -21,25 +47,7 @@ class GuestTablesController < ApplicationController
   end
 
   def show
-    @event = Event.find(params[:event_id])
     @table = GuestTable.find(params[:id])
-    @guest_choices = @event.guest_choices
-    guests = @event.guests.all.map { |g| g.name }
-    connection_1 = [@event.guests.all.map do |guest|
-                    {people: guest.guest_choice.choices - [guest.guest_choice.choices.last], weight: 6}
-                    end ]
-    connection_2 = [@event.guests.all.map do |guest|
-                    {people: guests - guest.guest_choice.choices + [guest.guest_choice.choices.last] + [guest.guest_choice.choices.first], weight: 1}
-                    end ]
-    connection_3 = [{people: @event.guests.where(relationship: "Family_B").map { |g| g.name }, weight: 5}]
-    connection_4 = [{people: @event.guests.where(relationship: "Family_G").map { |g| g.name }, weight: 5}]
-    connection_5 = [{people: @event.guests.where(relationship: "Friend_B").map { |g| g.name }, weight: 5}]
-    connection_6 = [{people: @event.guests.where(relationship: "Friend_G").map { |g| g.name }, weight: 5}]
-    connection_7 = [{people: guests, weight: 1}]
-
-    connections = connection_1.flatten + connection_2.flatten + connection_3 + connection_4 + connection_5 + connection_6 + connection_7
-    tables = [3, 20]
-    @seating = ORTools::Seating.new(connections: connections.flatten, tables: tables)
   end
 
   def destroy
