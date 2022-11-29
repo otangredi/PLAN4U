8528de51105c0d1c90fedfcb1338b2ac726eb6c3
anthonyborremans <anthonyborremans@hotmail.com>
2022-11-29 14:52:52 -0500

    fix url link rsvp

--- app/mailers/evite_mailer.rb
+++ app/mailers/evite_mailer.rb
@@ -3,9 +3,8 @@ class EviteMailer < ApplicationMailer
     @user = params[:user]
     @event = params[:event]
     @guest = params[:guest]
-    @url = "http://plan4you.herokuapp.com/event/#{@event.id}/rsvp"
+    @url = "http://plan4you.herokuapp.com/events/#{@event.id}/rsvp"
     attachments.inline['plan_for_you_logo.png'] = File.read('app/assets/images/plan_for_you_logo.png')
-    attachments.inline['landing_page.png'] = File.read('app/assets/images/landing_page.png')
     mail to: @guest.email, subject: 'Our Happily Even After'
   end
 end
