042cd896c611c3a09327c761074ddd22a7436a2f
hanbyeol kwon <janviertrois@naver.com>
2022-12-01 15:36:38 -0500

    val

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,5 @@ class Event < ApplicationRecord
   has_many :guest_tables, dependent: :destroy
   has_one :e_vite, dependent: :destroy
   has_many :guest_choices, through: :guests
+  validates :partner_first_name, :partner_last_name, :date, :venue, :name, presence: true
 end
